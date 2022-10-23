16329231b35d529d55d9b3a036fe70cd294a1ed0
Eden Liu <edenliu@mit.edu>
2022-10-23 14:54:26 -0400

    added comment router to index.ts

--- api/index.ts
+++ api/index.ts
@@ -11,6 +11,7 @@ import dotenv from 'dotenv';
 import * as userValidator from '../user/middleware';
 import {userRouter} from '../user/router';
 import {freetRouter} from '../freet/router';
+import {commentRouter} from '../comment/router';
 
 // Load environmental variables
 dotenv.config({});
@@ -76,6 +77,7 @@ app.get('/', (req: Request, res: Response) => {
 // Add routers from routes folder
 app.use('/api/users', userRouter);
 app.use('/api/freets', freetRouter);
+app.use('/api/comment', commentRouter);
 
 // Catch all the other routes and display error message
 app.all('*', (req: Request, res: Response) => {
